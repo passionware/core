23bcbb679fd822a1255dc1cf8d83b3c841d08320
Adam Borowski <fl.borovsky@gmail.com>
2024-08-03 13:59:41 +0200

    feat: useStreamValue

--- packages/streams/src/index.ts
+++ packages/streams/src/index.ts
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
-import { Observable } from 'rxjs';
+import { maybe, Maybe } from "@passionware/monads";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { Observable } from "rxjs";
 
 export const useStreamEvent = <T>(
   stream: Observable<T> | undefined,
-  callback: ((data: T) => void) | undefined
+  callback: ((data: T) => void) | undefined,
 ) => {
   useEffect(() => {
     if (stream && callback) {
@@ -12,4 +13,16 @@ export const useStreamEvent = <T>(
     }
     return () => undefined;
   }, [callback, stream]);
+};
+
+export const useStreamValue = <T>(stream: Observable<T>): Maybe<T> => {
+  const [value, setValue] = useState<Maybe<T>>(maybe.ofAbsent());
+  useLayoutEffect(() => {
+    if (stream) {
+      const subscription = stream.subscribe(setValue);
+      return () => subscription.unsubscribe();
+    }
+    return () => undefined;
+  }, [stream]);
+  return value;
 };
