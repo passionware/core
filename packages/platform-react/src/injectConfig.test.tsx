370a7beeba2f2cdbd30123e4bd3f6775109d5e2a
Adam Borowski <fl.borovsky@gmail.com>
2024-10-22 15:35:25 +0200

    feat(platform-react): injectConfig

---
+++ packages/platform-react/src/injectConfig.test.tsx
@@ -0,0 +1,62 @@
+/* @jest-environment jsdom */
+import { describe, it, expect, vi, afterEach, beforeEach } from "vitest";
+import { cleanup, render } from "@testing-library/react";
+import { ComponentType } from "react";
+import { injectConfig } from "./injectConfig";
+
+// A simple mock component to wrap
+const MockComponent: ComponentType<{
+  config: { configKey: string };
+  value: string;
+}> = ({ config, value }) => {
+  return <div>{`config-${config.configKey}-value-${value}`}</div>;
+};
+
+const mockInjectLogic = (api: any) => {
+  const props = api.useProps();
+  return {
+    configKey: `injected value from ${props.value}`,
+  };
+};
+
+const mockResolveTransformer = () => (props: any) => props;
+
+describe("injectConfig fromProps", () => {
+  it("should inject dynamic config based on props", () => {
+    const InjectedComponent = injectConfig(MockComponent)
+      .fromProps(mockInjectLogic)
+      .transformProps(mockResolveTransformer);
+
+    const screen = render(<InjectedComponent value="test value" />);
+
+    // Use a custom matcher to handle potential formatting issues
+    expect(
+      screen.getByText(
+        /config-injected value from test value-value-test value/,
+      ),
+    ).toBeInTheDocument();
+  });
+
+  it("should correctly apply prop transformations", () => {
+    const injectLogic = (api: any) => ({
+      configKey: `injected value from ${api.useProps().value}`,
+    });
+
+    const resolveTransformer = () => (props: { value: string }) => ({
+      value: `transformed-${props.value}`,
+    });
+
+    const InjectedComponent = injectConfig(MockComponent)
+      .fromProps(injectLogic)
+      .transformProps(resolveTransformer);
+
+    const screen = render(<InjectedComponent value="test value" />);
+
+    // Use a custom matcher to match the transformed props
+    expect(
+      screen.getByText(
+        /config-injected value from test value-value-transformed-test value/,
+      ),
+    ).toBeInTheDocument();
+  });
+});
